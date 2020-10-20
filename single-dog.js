224253594d7a691b014cc0a800cece89c782cddc
Nikos Amofa <nikosbright@gmail.com>
2020-10-20 05:29:50 -0400

    final commit assingment 2

--- single-dog.js
+++ single-dog.js
@@ -8,39 +8,34 @@ function watchForm() {
         event.preventDefault();
         //this is the user input
         let submissions = $('.breed-of-dogs').val();
-        submissions= submissions.toLowerCase();
+        submissions= submissions.toLowerCase();        
         //this make the image section visible        
         getTheDog(submissions)
     });
 };
 
-function getTheDog(submissions) {
-    const url = `https://dog.ceo/api/breed/${submissions}/images/random`;
-    fetch(url)
-    
+//This function calls the endpoint url and fetches the image from the API
+function getTheDog(submissions) {    
+    fetch(`https://dog.ceo/api/breed/${submissions}/images/random`)   
         .then(response => {
-            if (response.ok)
+            if (response.ok){
             return response.json()
-            throw Error()
-        })
+            }
+            throw new Error(response.statusText);
+        })         
         .then(getDog => displayResults(getDog))
-        .catch(error => alert('Sorry! Breed not found, please try again later.'));
+        .catch(error=> alert('Sorry! Breed not found, please try again later.'));
+       
 };
 
 
-function displayResults(getDog) {
-    $(".dog-images").html("");
+//This function displays the image to the DOM
+function displayResults(getDog) {    
 
-    $('.dog-images').append(`<input type="image" src="${getDog.message}" alt="picutre of a Dog" class="dogimages"/>`);
+    $('.dog-images').html(`<input type="image" src="${getDog.message}" alt="picutre of a Dog" class="dogimages">`);
     
     $('.hidden-div').removeAttr('hidden')
 };
 
 
-function all() {
-    console.log("The page has loaded")
-    watchForm();
-    
-};
-
-$(all);
+$(watchForm);
